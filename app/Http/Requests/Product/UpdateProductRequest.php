e6cdc76446a32352fb72366eb8dd1b950f1a82f8
martin <tribal.cz@gmail.com>
2023-06-20 12:50:15 +0200

    Přidány requesty pro produkty

---
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|unique:products,name,' . $this->route('product')->id,
+            'price' => 'required|numeric',
+            'qty' => 'required|numeric',
+            'supplier_id' => 'required|exists:suppliers,id',
+            'warehouse_id' => 'required|exists:warehouse,id',
+        ];
+    }
+}

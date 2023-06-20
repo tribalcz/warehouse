ee6d11ece1d70a6a0bbd626b403311287249e66a
martin <tribal.cz@gmail.com>
2023-06-20 12:51:19 +0200

    Přidány model pro Dodavatele

---
+++ app/Models/Supplier.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Supplier extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name', 'address'];
+
+    public function warehouses()
+    {
+        return $this->hasMany(Warehouse::class);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}

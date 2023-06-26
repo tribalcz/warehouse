8dfa83ff8861f315e77e7f7660156660fb4e211c
martin <tribal.cz@gmail.com>
2023-06-26 11:44:44 +0200

    Úprava migrace pro produkty, rozšíření o další sloupce.

--- database/migrations/2023_06_19_161815_create_product_table.php
+++ database/migrations/2023_06_19_161815_create_product_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
+            $table->string('url')->unique();
+            $table->string('description');
+            $table->text('content');
+            $table->integer('code')->comment('Vlastní kód produktu');
+            $table->bigInteger('ean')->comment('EAN kód produktu')->nullable(true);
+            $table->float('weight');
             $table->decimal('price', 8, 2);
             $table->integer('qty');
             $table->unsignedBigInteger('supplier_id');

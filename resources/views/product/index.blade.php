3f6150620acac2f8dd47574eb8819e8615e69480
martin <tribal.cz@gmail.com>
2023-06-26 11:50:32 +0200

    Drobné úpravy pohledů pro přehled a produktů

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -3,9 +3,11 @@
 @section('content')
     <div class="container">
         <h2>Seznam produktů</h2>
-        <a href="{{ route('products.create') }}" class="btn btn-primary mb-3">
-            <i class="fas fa-plus"></i> Přidat produkt
-        </a>
+        @if(Route::has('products.create'))
+            <a href="{{ route('products.create') }}" class="btn btn-primary mb-3">
+                <i class="fas fa-plus"></i> Přidat produkt
+            </a>
+        @endif
         <table class="table">
             <thead>
             <tr>
@@ -30,16 +32,25 @@
                         @endforeach
                     </td>
                     <td>
-                        <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-primary">
-                            <i class="fas fa-edit"></i>
-                        </a>
-                        <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="d-inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Opravdu chcete smazat tento produkt?')">
-                                <i class="fas fa-trash"></i>
-                            </button>
-                        </form>
+                        @if(Route::has('products.variant'))
+                            <a href="{{ route('products.variant', $product->id) }}" class="btn btn-sm btn-primary">
+                                <i class="fas fa-clone"></i>
+                            </a>
+                        @endif
+                        @if(Route::has('products.edit'))
+                            <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-primary">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                        @endif
+                        @if(Route::has('products.destroy'))
+                            <form action="{{ route('products.destroy', $product->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Opravdu chcete smazat tento produkt?')">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </form>
+                        @endif
                     </td>
                 </tr>
             @endforeach

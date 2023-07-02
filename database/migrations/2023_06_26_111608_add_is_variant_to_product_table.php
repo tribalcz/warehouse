771744c5800888039ed4431c8e45cddb3f93391f
martin <tribal.cz@gmail.com>
2023-07-02 12:45:32 +0200

    K migraci pro produkty přidán příznak isVariant

---
+++ database/migrations/2023_06_26_111608_add_is_variant_to_product_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->tinyInteger('isVariant')->default(0)->comment('Zda se jedná o variantu originálního produktu, Variantu lze vytvořit jen z originálního produktu')->after('qty');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};

4d67f612db1a7b5773e0ed0634e832286d0ccec3
Martin Suchodol <tribal.cz@gmail.com>
2023-08-17 00:49:34 +0200

    Přesun dílčích pohledů pro select s kategoriemi

--- resources/views/partials/category_select/categories.blade.php
+++ resources/views/partials/category_select/categories.blade.php
@@ -1,6 +1,6 @@
 @foreach($categories as $category)
     <option value="{{ $category->id }}">{{ $category->name }}</option>
     @foreach($category->childernCategories as $childCategory)
-        @include('partials.child_category', ['child_category' => $childCategory, 'indent' => 1])
+        @include('partials.category_select.child_category', ['child_category' => $childCategory, 'indent' => 1])
     @endforeach
 @endforeach
--- resources/views/partials/category_select/child_category.blade.php
+++ resources/views/partials/category_select/child_category.blade.php
@@ -1,4 +1,4 @@
 <option value="{{ $child_category->id }}">{{ str_repeat('--', $indent) }}{{ $child_category->name }}</option>
 @foreach($child_category->childernCategories as $childCategory)
-    @include('partials.child_category', ['child_category' => $childCategory, 'indent' => $indent+1])
+    @include('partials.category_select.child_category', ['child_category' => $childCategory, 'indent' => $indent+1])
 @endforeach

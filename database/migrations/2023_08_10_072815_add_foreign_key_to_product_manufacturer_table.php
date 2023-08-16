18b0da4b20f6d6ca4dead3c525adb4706be96a83
Martin Suchodol <tribal.cz@gmail.com>
2023-08-16 11:50:30 +0200

    Přidány migrace pro práci s výrobci

---
+++ database/migrations/2023_08_10_072815_add_foreign_key_to_product_manufacturer_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product_manufacturer', function (Blueprint $table) {
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->foreign('manufacturer_id')->references('id')->on('manufacturers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product_manufacturer', function (Blueprint $table) {
+            //
+        });
+    }
+};

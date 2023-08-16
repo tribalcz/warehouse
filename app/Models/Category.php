030330085fe9d755530605cb3e2a8de38d711713
Martin Suchodol <tribal.cz@gmail.com>
2023-08-16 21:30:23 +0200

    Přidán model pro kategorie

---
+++ app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function childernCategories()
+    {
+        return $this->hasMany(Category::class)->with('categories');
+    }
+}

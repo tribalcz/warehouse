1a2c84a34c085243f59c9706df9d88590fb36117
Martin Suchodol <tribal.cz@gmail.com>
2023-08-17 15:12:10 +0200

    Drobné úpravy selectu pro výběr kategorií

--- resources/views/category_select.blade.php
+++ resources/views/category_select.blade.php
@@ -1,5 +1,5 @@
 {{--TODO: přesunout mezi dílčí pohledy... asi, dále využívat jako @include soubor... asi --}}
-<select name="category_id" class="form-control">
+<select name="category_id[]" id="category" class="form-control" multiple required size="8">
     <option value="">-- Vyberte kategorii --</option>
     @include('partials.category_select.categories', ['partials.category_select.categories' => $categories, 'indent'=>0])
 </select>

d86b7ef842e4ef361da6d3fe73906f54e4383760
Martin Suchodol <tribal.cz@gmail.com>
2023-08-17 15:13:45 +0200

    Do moelu kategorií přidána metoda pro práci s produkty

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -20,4 +20,9 @@ class Category extends Model
     {
         return $this->hasMany(Category::class)->with('categories');
     }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }

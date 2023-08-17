52013e4f09de5c99523df9d07df7d532ff5630fd
Martin Suchodol <tribal.cz@gmail.com>
2023-08-17 09:48:49 +0200

    Deobná úprava partial pohledu pro přehled kategorí

--- resources/views/partials/category_manager/category_item.blade.php
+++ resources/views/partials/category_manager/category_item.blade.php
@@ -3,13 +3,13 @@
     <a href="{{ route('categories.edit', $category) }}">Upravit</a>
     <form action="{{ route('categories.destroy', $category) }}" method="post">
         @csrf
-        @method('delete')
+        @method('DELETE')
         <button type="submit">Smazat</button>
     </form>
     @if ($category->childernCategories)
     <ul>
         @foreach ($category->childernCategories as $childCategory)
-        @include('partials.category_manager.category_item', ['category' => $childCategory, 'indent' => '---'])
+        @include('partials.category_manager.category_item', ['category' => $childCategory, 'indent' => ''])
         @endforeach
     </ul>
     @endif
